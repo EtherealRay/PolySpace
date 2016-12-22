ac92ececdb0a867dbce893ed8d693134b8510139
Vincent <valentz936@gmail.com>
2016-12-22 21:12:48 +0700

    Render player

--- polyspace.py
+++ polyspace.py
@@ -1,19 +1,44 @@
 import arcade
- 
-SCREEN_WIDTH = 600
+import random
+
+SPRITE_SCALING = 0.5
+SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
- 
-class SpaceGameWindow(arcade.Window):
-    def __init__(self, width, height):
-        super().__init__(width, height)
- 
+BULLET_SPEED = 5
+
+class MyAppWindow(arcade.Window):
+    """ Main application class. """
+
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+        self.all_sprites_list = arcade.SpriteList()
+
+        self.score = 0
+        self.player_sprite = arcade.Sprite("images/ship.png", SPRITE_SCALING/2)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 70
+        self.all_sprites_list.append(self.player_sprite)
+
+
+
+
         arcade.set_background_color(arcade.color.BLACK)
- 
- 
+
     def on_draw(self):
         arcade.start_render()
+
+        self.all_sprites_list.draw()
+
+        output = "Score: {}".format(self.score)
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
  
  
-if __name__ == '__main__':
-    window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+def main():
+    MyAppWindow()
     arcade.run()
+
+
+if __name__ == "__main__":
+    main()
+
